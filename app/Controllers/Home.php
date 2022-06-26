e7bb65bce408f5ce164415188829a49b87aa973e
manjesh-amibba <41452410+manjesh-amibba@users.noreply.github.com>
2022-06-26 17:49:38 +0530

    UI data

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,9 @@ class Home extends BaseController
 {
     public function index()
     {
-        //return view('welcome_message');
-        echo "Welcome to Data Replication project";
+
+        echo view('header');
+        echo view('home');
+        echo view('footer');
     }
 }
